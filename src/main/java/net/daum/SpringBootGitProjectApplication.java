ba4e12e74eed39a7c57b312f84738c52c180bbc8
yoonziyeon <g_yeon94@naver.com>
2024-04-26 14:24:42 +0900

    최초로 만들어진 스프링부트 프로젝트 SpringBoot_git_Project를 모든 파일과 폴더를 한꺼번에 커밋해서 버전파일을 만듦

---
+++ src/main/java/net/daum/SpringBootGitProjectApplication.java
@@ -0,0 +1,13 @@
+package net.daum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitProjectApplication.class, args);
+	}
+
+}
